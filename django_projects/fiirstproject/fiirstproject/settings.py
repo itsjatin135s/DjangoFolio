f952ee5ce0697bb5bfcb57a835d8585a85265bce
Jatin Saini <itsjatin135@gmail.com>
2021-08-16 10:01:38 +0530

    Update settings.py

--- django_projects/fiirstproject/fiirstproject/settings.py
+++ django_projects/fiirstproject/fiirstproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-enlo6lg@fs@l9qx6whqzrt_kb(5d)25501_=p!h8z+xxalgj3+'
+SECRET_KEY = 'YOUR DJANGO SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.163','127.0.0.1:8000/']
+ALLOWED_HOSTS = ['127.0.0.1:8000/']
 
 
 # Application definition
